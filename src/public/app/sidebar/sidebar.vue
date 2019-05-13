9e4fe0b0c8dca4c4d8dde2c3131802031fe83324
Simon Malesys <simon.malesys@pasteur.fr>
2019-05-13 14:52:09 +0200

    Fix copy paste errors

--- src/public/app/sidebar/sidebar.vue
+++ src/public/app/sidebar/sidebar.vue
@@ -1,6 +1,6 @@
 <!--
-The individuals interface
-Copyright (C) 2017 Institut Pasteur
+Neanderthal Synapse
+Copyright (C) 2019 Institut Pasteur
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
